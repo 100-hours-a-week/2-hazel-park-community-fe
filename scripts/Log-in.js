c76717b2388057479319b7f0abbb642b9c13c69f
gmlwlsdl <heejin094@naver.com>
2024-10-28 14:56:51 +0900

    [#3] feat: input에 이벤트리스너 추가하여 입력값 바로 검사

--- scripts/Log-in.js
+++ scripts/Log-in.js
@@ -5,40 +5,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const submit = document.getElementById('submit')
   let emailHyperText = document.getElementById('email-hyper-text')
   let pwHyperText = document.getElementById('pw-hyper-text')
-  let emailCheck = false
-  let pwCheck = false
+
+  function validateForm() {
+    let emailCheck = false
+    let pwCheck = false
+
+    if (!inputEmail.value.trim() || !emailValidCheck(inputEmail.value.trim())) {
+      emailHyperText.innerText = '올바른 이메일 주소 형식을 입력해주세요.'
+      emailHyperText.style.visibility = 'visible'
+    } else {
+      emailCheck = true
+      emailHyperText.style.visibility = 'hidden'
+    }
+
+    if (!inputPassword.value.trim()) {
+      pwHyperText.style.visibility = 'visible'
+      pwHyperText.innerText = '비밀번호를 입력해주세요.'
+    } else if (!pwValidCheck(inputPassword.value.trim())) {
+      pwHyperText.innerText =
+        '비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.'
+      pwHyperText.style.visibility = 'visible'
+    } else {
+      pwCheck = true
+      pwHyperText.style.visibility = 'hidden'
+    }
+
+    if (emailCheck && pwCheck) {
+      submit.style.backgroundColor = '#7f6aee'
+      submit.style.cursor = 'pointer'
+      return true
+    }
+  }
 
   if (submit) {
     submit.addEventListener('click', (event) => {
       event.preventDefault()
-
-      emailHyperText.innerText = ''
-      pwHyperText.innerText = ''
-
-      if (
-        !inputEmail.value.trim() ||
-        !emailValidCheck(inputEmail.value.trim())
-      ) {
-        emailHyperText.innerText = '올바른 이메일 주소 형식을 입력해주세요.'
-        emailHyperText.style.visibility = 'visible'
-      } else {
-        emailCheck = true
-      }
-
-      if (!inputPassword.value.trim()) {
-        pwHyperText.style.visibility = 'visible'
-        pwHyperText.innerText = '비밀번호를 입력해주세요.'
-      } else if (!pwValidCheck(inputPassword.value.trim())) {
-        pwHyperText.innerText =
-          '비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.'
-        pwHyperText.style.visibility = 'visible'
-      } else {
-        pwCheck = true
-      }
-
-      if (emailCheck && pwCheck) {
-        submit.style.backgroundColor = '#7f6aee'
-        submit.style.cursor = 'pointer'
+      if (validateForm()) {
+        // TODO: 추후 로그인 api 연동 추가
+        handleNavigation('/2-hazel-park-community-fe/html/Posts.html')
       }
     })
   }
@@ -49,16 +53,12 @@ document.addEventListener('DOMContentLoaded', () => {
     )
   }
 
-  function emailValidCheck(email) {
-    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/
+  inputEmail.addEventListener('input', validateForm)
+  inputPassword.addEventListener('input', validateForm)
 
-    if (pattern.test(email) === false) {
-      console.log('false')
-      return false
-    } else {
-      console.log('true')
-      return true
-    }
+  function emailValidCheck(email) {
+    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+$/
+    return pattern.test(email)
   }
 
   function pwValidCheck(value) {
