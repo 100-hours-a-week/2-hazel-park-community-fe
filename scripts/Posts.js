79e98be068856bfe39a6c340f0e63eb0496a2b2b
gmlwlsdl <heejin094@naver.com>
2024-10-28 10:04:23 +0900

    [#1] feat: (임시) 로그아웃시 프로필 이미지 visibility hidden

--- scripts/Posts.js
+++ scripts/Posts.js
@@ -2,28 +2,40 @@ document.addEventListener('DOMContentLoaded', () => {
   const postBtn = document.getElementById('add-post-button')
   const profileImg = document.getElementById('profile-img')
   const profileDropdown = document.getElementById('profile-dropdown')
-  const profileDropdownEditProfile = document.getElementById(
-    'profile-dropdown-edit-profile',
-  )
-  const profileDropdownEditPassword = document.getElementById(
-    'profile-dropdown-edit-password',
-  )
+  const dropdownEditProfile = document.getElementById('dropdown-edit-profile')
+  const dropdownEditPassword = document.getElementById('dropdown-edit-password')
+  const dropdownLogout = document.getElementById('dropdown-logout')
 
   if (postBtn) {
     postBtn.addEventListener('click', handlePostBtn)
   }
+
   if (profileImg) {
-    profileImg.addEventListener('click', () => {
+    profileImg.addEventListener('click', (event) => {
+      event.stopPropagation()
       profileDropdown.style.visibility =
         profileDropdown.style.visibility === 'visible' ? 'hidden' : 'visible'
     })
   }
+
   if (profileImg) {
     document.addEventListener('click', (event) => {
       if (!profileImg.contains(event.target)) {
         profileDropdown.style.visibility = 'hidden'
       }
     })
+  }
+
+  if (dropdownEditProfile) {
+    dropdownEditProfile.addEventListener('click', handleDropdownEditProfile)
+  }
+
+  if (dropdownEditPassword) {
+    dropdownEditPassword.addEventListener('click', handleDropdownEditPassword)
+  }
+
+  if (dropdownLogout) {
+    dropdownLogout.addEventListener('click', handleDropdownLogout(profileImg))
   }
 })
 
@@ -33,4 +45,18 @@ function handlePostBtn() {
 
 function handlePostItem() {
   window.location.href = '/2-hazel-park-community-fe/html/post.html'
+}
+
+function handleDropdownEditProfile() {
+  window.location.href = '/2-hazel-park-community-fe/html/edit-profile.html'
+}
+
+function handleDropdownEditPassword() {
+  window.location.href = '/2-hazel-park-community-fe/html/edit-password.html'
+}
+
+function handleDropdownLogout(profileImg) {
+  if (profileImg) {
+    profileImg.style.visibility = 'hidden'
+  }
 }
