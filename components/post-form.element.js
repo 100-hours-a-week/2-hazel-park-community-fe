32d737c37419e148ded89dd7e932d67da9d523e0
gmlwlsdl <heejin094@naver.com>
2024-10-29 14:01:11 +0900

    [#10] feat: 게시글 제목 27자 제한

--- components/post-form.element.js
+++ components/post-form.element.js
@@ -1,3 +1,5 @@
+import handleNavigation from '../utils/navigation.js'
+
 class postFormElement extends HTMLElement {
   constructor() {
     super()
@@ -8,6 +10,7 @@ class postFormElement extends HTMLElement {
   connectedCallback() {
     this.checkLocation()
     this.shadowRoot.innerHTML = this.template()
+    this.addEventListener()
   }
 
   template() {
@@ -22,14 +25,14 @@ class postFormElement extends HTMLElement {
                   ? '<div>' +
                     '<div class="input-title">제목*</div>' +
                     '<input id="input-title" type="text" placeholder="제목을 입력해주세요. (최대 26글자)" class="input-value" />' +
-                    '<div id="img-hyper-text" style="height: 1.7em; visibility: hidden;" class="hyper-text"></div>' +
-                    '<input id="input-profile-img" type="file" class="input-profile-img" accept="image/*" />' +
+                    '<div id="title-hyper-text" style="height: 1.7em; visibility: hidden;" class="hyper-text"></div>' +
                     '</div>' +
                     '<div class="email-wrap">' +
                     '<div class="input-title">내용*</div>' +
-                    '<textarea id="input-content" type="text" placeholder="내용을 입력해주세요." class="input-value input-value-textarea"></textarea>' +
+                    '<textarea id="input-contents" type="text" placeholder="내용을 입력해주세요." class="input-value input-value-textarea"></textarea>' +
+                    '<div id="contents-hyper-text" style="height: 1.7em; visibility: hidden;" class="hyper-text">dfsfs</div>' +
                     '</div>' +
-                    '<div style="margin-top: 1rem" class="nickname-wrap">' +
+                    '<div class="nickname-wrap">' +
                     '<div class="input-title">이미지</div>' +
                     '<div class="input-file-wrap"><input id="input-nickname" type="file" class="input-value-file"/>' +
                     '<label for="input-nickname" class="input-file-label">파일 선택</label>' +
@@ -51,6 +54,81 @@ class postFormElement extends HTMLElement {
                 <input id="submit" type="submit" value="완료" class="make-post-submit" />
             </form>
         </div>`
+  }
+
+  addEventListener() {
+    const inputTitle = this.shadowRoot.getElementById('input-title')
+    const inputContents = this.shadowRoot.getElementById('input-contents')
+    const submit = this.shadowRoot.getElementById('submit')
+
+    if (inputTitle) {
+      inputTitle.addEventListener('input', () => this.validateForm())
+    }
+
+    if (inputContents) {
+      inputContents.addEventListener('input', () => this.validateForm())
+    }
+
+    if (submit) {
+      submit.addEventListener('click', (event) => {
+        event.preventDefault()
+        if (this.validateForm() === 'posts') {
+          handleNavigation('/html/Posts.html')
+        }
+      })
+    }
+  }
+
+  validateForm() {
+    const inputTitle = this.shadowRoot.getElementById('input-title')
+    const inputContents = this.shadowRoot.getElementById('input-contents')
+    const titleHyperText = this.shadowRoot.getElementById('title-hyper-text')
+    const contentsHyperText = this.shadowRoot.getElementById(
+      'contents-hyper-text',
+    )
+    const submit = this.shadowRoot.getElementById('submit')
+    submit.style.backgroundColor = '#aea0eb'
+    submit.style.cursor = 'not-allowed'
+
+    if (titleHyperText) {
+      titleHyperText.innerText = ''
+    }
+
+    if (contentsHyperText) {
+      contentsHyperText.innerText = ''
+    }
+
+    let titleCheck = false
+    let contentsCheck = false
+
+    if (!inputTitle.value.trim()) {
+      titleCheck = false
+      titleHyperText.innerText = '제목, 내용을 모두 작성해주세요.'
+      titleHyperText.style.visibility = 'visible'
+    } else if (inputTitle.value.length > 27) {
+      titleCheck = false
+      titleHyperText.innerText = '제목은 27자 이하로 작성해주세요.'
+      titleHyperText.style.visibility = 'visible'
+      inputTitle.value = inputTitle.value.slice(0, 27)
+    } else {
+      titleCheck = true
+      titleHyperText.style.visibility = 'hidden'
+    }
+
+    if (!inputContents.value.trim()) {
+      contentsCheck = false
+      contentsHyperText.innerText = '제목, 내용을 모두 작성해주세요.'
+      contentsHyperText.style.visibility = 'visible'
+    } else {
+      contentsCheck = true
+      contentsHyperText.style.visibility = 'hidden'
+    }
+
+    if (titleCheck && contentsCheck) {
+      submit.style.backgroundColor = '#7f6aee'
+      submit.style.cursor = 'pointer'
+      return 'posts'
+    }
   }
 
   checkLocation() {
