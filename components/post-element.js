9797cae165438c06bc148bfc85ea79103c1a680f
gmlwlsdl <heejin094@naver.com>
2024-10-29 16:01:41 +0900

    [#15] feat: 글 내용 섹션 추가

--- components/post-element.js
+++ components/post-element.js
@@ -8,89 +8,63 @@ class PostElement extends HTMLElement {
   }
 
   connectedCallback() {
-    this.render()
+    this.shadowRoot.innerHTML = this.template()
   }
 
-  disconnectedCallback() {
-    // 컴포넌트가 DOM에서 제거될 때 수행할 작업이 있다면 여기에 작성
-  }
-
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    // 속성 값이 변경될 때 수행할 작업이 있다면 여기에 작성
-  }
-
-  adoptedCallback() {
-    // 컴포넌트가 새로운 DOM으로 이동될 때 수행할 작업이 있다면 여기에 작성
-  }
-
-  render() {
-    const posts = [
-      {
-        id: 1,
-        title: '첫 번째 게시글의최대제목수수수',
-        likes: 1000,
-        comments: 10000,
-        views: 100000,
-        updateAt: '2024-10-26 22:25:00',
-        writer: 'user1',
-      },
-      {
-        id: 2,
-        title: '두 번째 게시글',
-        likes: 0,
-        comments: 2,
-        views: 3,
-        updateAt: '2024-10-26 22:24:01',
-        writer: 'user2',
-      },
-      {
-        id: 3,
-        title: '세 번째 게시글',
-        likes: 3,
-        comments: 1,
-        views: 5,
-        updateAt: '2024-10-26 22:20:10',
-        writer: 'user3',
-      },
-    ]
-
-    this.shadowRoot.innerHTML = this.template(posts)
-
-    const postItems = this.shadowRoot.querySelectorAll('.post-item')
-    postItems.forEach((item) => {
-      item.addEventListener('click', () => handleNavigation('/html/post.html'))
-    })
-  }
-
-  template(posts) {
+  template() {
     return `
-          <link rel="stylesheet" href="../styles/Posts.css" />
-          <div class="post-list">
-            ${posts
-              .map(
-                (post) => `
-                  <div class="post-item">
-                      <div class="post-info-wrap">
-                          <div class="post-info-wrap-left">
-                              <div class="post-title">${post.title}</div>
-                              <div class="post-wrap-detail">
-                                  <div class="post-likes">좋아요 ${checkCount(post.likes)}</div>
-                                  <div class="post-comment">댓글 ${checkCount(post.comments)}</div>
-                                  <div class="post-views">조회수 ${checkCount(post.views)}</div>                        
-                              </div>
-                          </div>
-                          <div class="post-updateAt">${post.updateAt}</div>
-                      </div>
-                      <div class="post-writer-wrap">
-                        <div class="post-writer-img"></div>
-                        <div class="post-writer">${post.writer}</div>                  
-                      </div>
-                  </div>
-                `,
-              )
-              .join('')}
-          </div>
-        `
+        <link rel="stylesheet" href="../styles/post.css" />
+        <section class="post">
+            <article class="post-detail-top">
+              <div class="post-title">제목</div>
+              <div class="post-title-detail-wrap">
+                <div class="post-writer-img"></div>
+                <div class="post-writer-name">작성자</div>
+                <div class="post-updateAt">yyyy-mm-dd hh:mm:ss</div>
+                <div class="post-controll-button">
+                  <button id="button-update" class="post-controll-button-detail">
+                    수정
+                  </button>
+                  <button id="button-delete" class="post-controll-button-detail">
+                    삭제
+                  </button>
+                </div>
+              </div>
+            </article>
+            <article class="post-detail-bottom">
+              <div class="post-contents-img"></div>
+              <div class="post-contents">
+                무엇을 얘기할까요? 아무말이라면, 삶은 항상 놀라운 모험이라고
+                생각합니다. 우리는 매일 새 로운 경험을 하고 배우며 성장합니다. 때로는
+                어려움과 도전이 있지만, 그것들이 우리를 더 강 하고 지혜롭게 만듭니다.
+                또한 우리는 주변의 사람들과 연결되며 사랑과 지지를 받습니다. 그래 서
+                우리의 삶은 소중하고 의미가 있습니다. 자연도 아름다운 이야기입니다.
+                우리 주변의 자연은 끝없는 아름다움과 신비로움을 담고 있습 니다. 산,
+                바다, 숲, 하늘 등 모든 것이 우리를 놀라게 만들고 감동시킵니다. 자연은
+                우리의 생명 과 안정을 지키며 우리에게 힘을 주는 곳입니다. 마지막으로,
+                지식을 향한 탐구는 항상 흥미로운 여정입니다. 우리는 끝없는 지식의
+                바다에서 배 우고 발견할 수 있으며, 이것이 우리를 더 깊이 이해하고
+                세상을 더 넓게 보게 해줍니다. 그런 의미에서, 삶은 놀라움과
+                경이로움으로 가득 차 있습니다. 새로운 경험을 즐기고 항상 앞 으로
+                나아가는 것이 중요하다고 생각합니다.
+              </div>
+              <div class="post-interaction">
+                <div class="post-interaction-box">
+                  <div class="post-interaction-value">${checkCount(1000)}</div>
+                  <div class="post-interaction-title">좋아요</div>
+                </div>
+                <div class="post-interaction-box">
+                  <div class="post-interaction-value">${checkCount(10000)}</div>
+                  <div class="post-interaction-title">조회수</div>
+                </div>
+                <div class="post-interaction-box">
+                  <div class="post-interaction-value">${checkCount(100000)}</div>
+                  <div class="post-interaction-title">댓글</div>
+                </div>
+              </div>
+            </article>
+        </section>
+    `
   }
 }
 
---
+++ components/post-list-element.js
@@ -0,0 +1,97 @@
+import checkCount from '../utils/check-count.js'
+import handleNavigation from '../utils/navigation.js'
+
+class PostListElement extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  disconnectedCallback() {
+    // 컴포넌트가 DOM에서 제거될 때 수행할 작업이 있다면 여기에 작성
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    // 속성 값이 변경될 때 수행할 작업이 있다면 여기에 작성
+  }
+
+  adoptedCallback() {
+    // 컴포넌트가 새로운 DOM으로 이동될 때 수행할 작업이 있다면 여기에 작성
+  }
+
+  render() {
+    const posts = [
+      {
+        id: 1,
+        title: '첫 번째 게시글의최대제목수수수',
+        likes: 1000,
+        comments: 10000,
+        views: 100000,
+        updateAt: '2024-10-26 22:25:00',
+        writer: 'user1',
+      },
+      {
+        id: 2,
+        title: '두 번째 게시글',
+        likes: 0,
+        comments: 2,
+        views: 3,
+        updateAt: '2024-10-26 22:24:01',
+        writer: 'user2',
+      },
+      {
+        id: 3,
+        title: '세 번째 게시글',
+        likes: 3,
+        comments: 1,
+        views: 5,
+        updateAt: '2024-10-26 22:20:10',
+        writer: 'user3',
+      },
+    ]
+
+    this.shadowRoot.innerHTML = this.template(posts)
+
+    const postItems = this.shadowRoot.querySelectorAll('.post-item')
+    postItems.forEach((item) => {
+      item.addEventListener('click', () => handleNavigation('/html/post.html'))
+    })
+  }
+
+  template(posts) {
+    return `
+          <link rel="stylesheet" href="../styles/Posts.css" />
+          <div class="post-list">
+            ${posts
+              .map(
+                (post) => `
+                  <div class="post-item">
+                      <div class="post-info-wrap">
+                          <div class="post-info-wrap-left">
+                              <div class="post-title">${post.title}</div>
+                              <div class="post-wrap-detail">
+                                  <div class="post-likes">좋아요 ${checkCount(post.likes)}</div>
+                                  <div class="post-comment">댓글 ${checkCount(post.comments)}</div>
+                                  <div class="post-views">조회수 ${checkCount(post.views)}</div>                        
+                              </div>
+                          </div>
+                          <div class="post-updateAt">${post.updateAt}</div>
+                      </div>
+                      <div class="post-writer-wrap">
+                        <div class="post-writer-img"></div>
+                        <div class="post-writer">${post.writer}</div>                  
+                      </div>
+                  </div>
+                `,
+              )
+              .join('')}
+          </div>
+        `
+  }
+}
+
+customElements.define('post-list-element', PostListElement)
