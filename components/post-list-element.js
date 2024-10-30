bbf666e0f63f89d84b206f2a74e5f95f10e80ac9
gmlwlsdl <heejin094@naver.com>
2024-10-30 15:14:03 +0900

    feat: post 리스트 출력

--- components/post-list-element.js
+++ components/post-list-element.js
@@ -5,54 +5,16 @@ class PostListElement extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
+    this.posts = JSON.parse(localStorage.getItem('post')) || []
   }
 
   connectedCallback() {
     this.render()
   }
 
-  disconnectedCallback() {
-    // 컴포넌트가 DOM에서 제거될 때 수행할 작업이 있다면 여기에 작성
-  }
-
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    // 속성 값이 변경될 때 수행할 작업이 있다면 여기에 작성
-  }
-
-  adoptedCallback() {
-    // 컴포넌트가 새로운 DOM으로 이동될 때 수행할 작업이 있다면 여기에 작성
-  }
-
   render() {
-    const posts = [
-      {
-        id: 1,
-        title: '첫 번째 게시글의최대제목수수수',
-        likes: 1000,
-        comments: 10000,
-        views: 100000,
-        updateAt: '2024-10-26 22:25:00',
-        writer: 'user1',
-      },
-      {
-        id: 2,
-        title: '두 번째 게시글',
-        likes: 0,
-        comments: 2,
-        views: 3,
-        updateAt: '2024-10-26 22:24:01',
-        writer: 'user2',
-      },
-      {
-        id: 3,
-        title: '세 번째 게시글',
-        likes: 3,
-        comments: 1,
-        views: 5,
-        updateAt: '2024-10-26 22:20:10',
-        writer: 'user3',
-      },
-    ]
+    const posts = Array.isArray(this.posts) ? this.posts : [this.posts]
+    console.log(posts)
 
     this.shadowRoot.innerHTML = this.template(posts)
 
@@ -72,18 +34,18 @@ class PostListElement extends HTMLElement {
                   <div class="post-item">
                       <div class="post-info-wrap">
                           <div class="post-info-wrap-left">
-                              <div class="post-title">${post.title}</div>
+                              <div class="post-title">${post.post_title}</div>
                               <div class="post-wrap-detail">
-                                  <div class="post-likes">좋아요 ${checkCount(post.likes)}</div>
-                                  <div class="post-comment">댓글 ${checkCount(post.comments)}</div>
-                                  <div class="post-views">조회수 ${checkCount(post.views)}</div>                        
+                                  <div class="post-likes">좋아요 ${checkCount(post.post_likes)}</div>
+                                  <div class="post-comment">댓글 ${checkCount(post.post_comments)}</div>
+                                  <div class="post-views">조회수 ${checkCount(post.post_views)}</div>                        
                               </div>
                           </div>
-                          <div class="post-updateAt">${post.updateAt}</div>
+                          <div class="post-updateAt">${new Date(post.post_updatedAt).toLocaleString()}</div>
                       </div>
                       <div class="post-writer-wrap">
                         <div class="post-writer-img"></div>
-                        <div class="post-writer">${post.writer}</div>                  
+                        <div class="post-writer">${post.post_writer}</div>                  
                       </div>
                   </div>
                 `,
